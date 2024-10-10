7eea7b094ce8745700922d69b1a2fcbccf1cd81a
Markus-Stuppnig <markus@stuppnig.net>
2024-10-10 16:58:40 +0200

    Adds hibernate dialect

--- src/main/java/org/example/JdbcConfiguration.java
+++ src/main/java/org/example/JdbcConfiguration.java
@@ -1,11 +1,16 @@
 package org.example;
 
+import jakarta.persistence.EntityManagerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 
 import javax.sql.DataSource;
+import java.util.Properties;
 
 @Configuration
 public class JdbcConfiguration {
@@ -14,7 +19,7 @@ public class JdbcConfiguration {
     public DataSource dataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName("org.postgresql.Driver");
-        dataSource.setUrl("jdbc:postgresql://localhost:5432/db");
+        dataSource.setUrl("jdbc:postgresql://db:5432/db");
         dataSource.setUsername("postgres");
         dataSource.setPassword("postgres");
         return dataSource;
@@ -23,5 +28,35 @@ public class JdbcConfiguration {
     @Bean
     public NamedParameterJdbcTemplate namedParameterJdbcTemplate(DataSource dataSource) {
         return new NamedParameterJdbcTemplate(dataSource);
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource dataSource) {
+        LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
+        em.setDataSource(dataSource);
+
+        // Set JPA Vendor Adapter (Hibernate in this case)
+        HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+        em.setJpaVendorAdapter(vendorAdapter);
+
+        // Set Hibernate properties
+        Properties properties = new Properties();
+        properties.setProperty("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
+        properties.setProperty("hibernate.hbm2ddl.auto", "update");
+        properties.setProperty("hibernate.show_sql", "true");
+
+        em.setJpaProperties(properties);
+
+        // Specify the packages to scan for entity classes
+        em.setPackagesToScan("org.example.entities");  // Adjust package name to your project structure
+
+        return em;
+    }
+
+    @Bean
+    public JpaTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
+        JpaTransactionManager transactionManager = new JpaTransactionManager();
+        transactionManager.setEntityManagerFactory(entityManagerFactory);
+        return transactionManager;
     }
 }
