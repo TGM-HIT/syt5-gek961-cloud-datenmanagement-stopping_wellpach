6c03654beb4da899ffabaf5dbabfe8cdbb56e566
Markus-Stuppnig <markus@stuppnig.net>
2024-10-10 16:09:09 +0200

    Adds roles

--- src/main/java/org/example/user/authentication/AuthenticationController.java
+++ src/main/java/org/example/user/authentication/AuthenticationController.java
@@ -1,11 +1,14 @@
 package org.example.user.authentication;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.AllArgsConstructor;
 import org.example.user.UserRepository;
 import org.example.user.UserService;
 import org.example.user.authentication.config.AuthenticationService;
 import org.example.user.authentication.protocols.request.AuthenticationRequest;
 import org.example.user.authentication.protocols.request.RegisterRequest;
+import org.example.user.entities.Role;
+import org.example.user.entities.User;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,14 +21,19 @@ import java.io.IOException;
 public class AuthenticationController {
 
     private final AuthenticationService service;
+    private final UserService userService;
 
     @PostMapping("/register")
-    public ResponseEntity<?> register(@RequestBody RegisterRequest request) {
-        try {
-            return service.register(request);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+    public ResponseEntity<?> changeEmail(HttpServletRequest request, @RequestBody RegisterRequest registerRequest) {
+
+        User user = userService.getUserFromRequest(request);
+        if (user == null) return null;
+
+        if(user.getRole() != Role.ADMIN) {
+            return ResponseEntity.status(403).body("Forbidden! Not Admin!");
         }
+
+        return service.register(registerRequest);
     }
 
     @PostMapping("/authenticate")
--- src/main/java/org/example/user/authentication/config/AuthenticationService.java
+++ src/main/java/org/example/user/authentication/config/AuthenticationService.java
@@ -27,7 +27,7 @@ public class AuthenticationService {
     private final JwtService jwtService;
     private final AuthenticationManager authenticationManager;
 
-    public ResponseEntity<?> register(RegisterRequest request) throws IOException {
+    public ResponseEntity<?> register(RegisterRequest request) {
 
         if(this.userRepository.findByUserid(request.getUserid()).isPresent()) {
             System.out.println("User with userid: " + request.getUserid() + " already exists!");
@@ -35,21 +35,39 @@ public class AuthenticationService {
             return ResponseEntity.status(409).body("User with email: " + request.getUserid() + " already exists!");
         }
 
+        System.out.println("Name: " + request.getName());
+
+        Role role = Role.READER;
+        switch(request.getRole()) {
+            case "admin":
+                role = Role.ADMIN;
+                break;
+
+            case "moderator":
+                role = Role.MODERATOR;
+                break;
+
+            case "reader":
+                role = Role.READER;
+                break;
+        }
+
         var user = User.builder()
                 .name(request.getName())
                 .userid(request.getUserid())
                 .password(passwordEncoder.encode(request.getPassword()))
-                .role(Role.ADMIN)
+                .role(role)
                 .build();
 
         userRepository.save(user);
 
-        var jwtToken = jwtService.generateToken(user);
+//        var jwtToken = jwtService.generateToken(user);
+//
+//        HashMap<String, String> map = new HashMap<>();
+//        map.put("token", jwtToken);
 
-        HashMap<String, String> map = new HashMap<>();
-        map.put("token", jwtToken);
-
-        return ResponseEntity.ok().body(JSONObject.toJSONString(map));
+//        return ResponseEntity.ok().body(JSONObject.toJSONString(map));
+        return ResponseEntity.ok().body("worked");
     }
 
     public ResponseEntity<?> authenticate(AuthenticationRequest request) {
